172a906b6450a0cc5f7bf135ea1c9e57a0744720
DRem <drem.aimlf@gmail.com>
2017-02-01 01:56:45 +0300

    add editor for article

--- src/components/editor/controller.js
+++ src/components/editor/controller.js
@@ -1,3 +1,33 @@
-/**
- * Created by DRem on 2/1/17.
- */
+class EditorCtrl {
+
+    constructor(ArticleService, article, $state) {
+        "ngInject";
+
+        this._ArticleService = ArticleService;
+        this._$state = $state;
+
+        if (!article) {
+            this.article = {
+                title: "",
+                author: "",
+                text: ""
+            };
+        } else {
+            this.article = article;
+        }
+    };
+
+    submit() {
+        this.isSubmitting = true;
+
+        this._ArticleService.save(this.article)
+            .then(newArticle => this._$state.go("app.article", {slug: newArticle.slug}))
+            .catch(err => {
+                this.isSubmitting = false;
+                this.errors = err.data.errors;
+            });
+    };
+
+};
+
+export default EditorCtrl;
